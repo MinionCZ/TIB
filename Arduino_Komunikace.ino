fac24486c733eff4d1d034228cb386c53fdfe045
MinionCZ <43399650+MinionCZ@users.noreply.github.com>
2020-05-17 19:37:25 +0200

    Add files via upload

--- Arduino_Komunikace.ino
+++ Arduino_Komunikace.ino
@@ -2,6 +2,8 @@ int piny[8];
 bool blik = false;
 bool sviti = false;
 uint32_t posledniMillis = millis();
+String hi = "Ahoj";
+
 void setup() {
   // put your setup code here, to run once:
   for (int i = 0; i < 8; i++) {
@@ -9,25 +11,15 @@ void setup() {
     pinMode(piny[i], OUTPUT);
   }
   Serial.begin(9600);
+  Serial.println(hi);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  while (Serial.available() != 0) {
-    char ch = (char) Serial.read();
-    switch (ch) {
-      case 'r':
-        rozsvit(8);
-        break;
-      case 'z':
-        zhasni();
-        break;
-      case 'b':
-        blik = !blik;
-        break;
-      default:
-        break;
-    }
+  String s = prectiString();
+  if(!s.equals("")){
+    Serial.println(s);
+    vyhodnot(s);
   }
   blikej();
   delay(1);
@@ -56,6 +48,42 @@ void blikej() {
     }
   }
 }
-
-
-
+String prectiString(){
+  if(Serial.available() == 0){
+    return "";
+  }
+  String packet = "";
+  int pocitadlo = 0;
+  while(pocitadlo<Serial.available()){
+    pocitadlo = Serial.available();
+    delay(2);
+  }
+  while(Serial.available()!=0){
+    packet += (char)Serial.read();
+  }
+  return packet;
+}
+void vyhodnot(String prikaz){
+  if(prikaz.length()<2){
+    Serial.println("Error");
+    return;
+  }
+  if(prikaz.length()==2 && prikaz.equals("AT")){
+    Serial.println("OK");
+    return;
+  }
+  String atHlavicka = prikaz.substring(0, 3);
+  if(atHlavicka.equals("AT+")){
+    prikaz.remove(0,3);
+    if(prikaz.equals("ON")){
+      rozsvit(8);
+    }else if(prikaz.equals("OFF")){
+      zhasni();
+    }
+    
+  }else{
+    Serial.println("Error");
+  }
+  
+  
+}
