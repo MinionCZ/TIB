27f5909737b08d854dedb2c5a5b8d619f6a830f2
MinionCZ <43399650+MinionCZ@users.noreply.github.com>
2020-05-28 18:03:42 +0200

    Add files via upload

--- funguje_to.ino
+++ funguje_to.ino
@@ -10,6 +10,7 @@ struct Hodiny {
 } cas;
 
 void setup() {
+  Serial.begin(9600);
   pinMode(7, OUTPUT);
   lcd.begin(16, 2);
   cas.hodina = 17;
@@ -26,6 +27,7 @@ void loop() {
   lcd.print(":");
   lcd.print(cas.sekunda);
   tik();
+  nastaveni();
   delay(1);
 }
 void tik() {
@@ -44,4 +46,75 @@ void tik() {
       }
     }
   }
+}
+void nastaveni() {
+  if (Serial.available() == 0) {
+    return;
+  }
+  String s = "";
+  while (Serial.available() > 0) {
+    s += (char) Serial.read();
+  }
+  int vysledek = over(s);
+  switch (vysledek) {
+    case -1:
+      Serial.println("Error");
+      return;
+    case 1:
+      // to do
+      break;
+    case 2:
+      Serial.println(vratInfo(s));
+      break;
+    default:
+      break;
+  }
+
+}
+/*
+   param prikaz - textovy retezec od uzivatele
+   return pokud je prikaz nespravny -> -1
+          pokud je prikaz set -> 1
+          pokud je prikaz get -> 2
+*/
+int over(String prikaz) {
+  if (prikaz.length() >= 3) {
+    String substr = prikaz.substring(0, 3);
+    if (substr.equals("set")) {
+      return 1;
+    } else if (substr.equals("get")) {
+      return 2;
+    } else {
+      return -1;
+    }
+  } else {
+    return -1;
+  }
+}
+/*
+   param prikaz - string co se ma udelat
+   return String -> pokud je prikaz time -> cas
+                 -> pokud je prikaz text -> text
+                 -> pokud neco jineho -> error
+*/
+
+String vratInfo(String prikaz) {
+  String substr = prikaz.substring(3);
+  if (substr.length() != 4) {
+    return "Error";
+  }
+  if (substr.equals("text")) {
+    return "Donde esta la biblioteca?";
+  } else if (substr.equals("time")) {
+    String tim = "";
+    tim += cas.hodina;
+    tim += ":";
+    tim += cas.minuta;
+    tim += ":";
+    tim += cas.sekunda;
+    return tim;
+  } else {
+    return "Error";
+  }
+
 }
